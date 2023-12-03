8fb7714279cc47ffe5f616ae743ffc680f256473
LOU Xun <aquarhead@gmail.com>
2023-12-03 11:20:54 +0000

    2023 day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -2,9 +2,10 @@ use std::collections::{HashMap, HashSet};
 
 type Pos = (i32, i32);
 
-pub fn solve(input: String) -> u32 {
+pub fn solve(input: String) -> (u32, u32) {
   let mut map = HashMap::<Pos, char>::new();
   let mut symbol_pos = Vec::<Pos>::new();
+  let mut gear_pos = Vec::<Pos>::new();
 
   for (x, line) in input.trim().lines().enumerate() {
     for (y, char) in line.trim().char_indices() {
@@ -14,7 +15,12 @@ pub fn solve(input: String) -> u32 {
         x if x >= '0' && x <= '9' => {
           let _ = map.insert(pos, char);
         }
-        _symbol => symbol_pos.push(pos),
+        s => {
+          symbol_pos.push(pos);
+          if s == '*' {
+            gear_pos.push(pos);
+          }
+        }
       }
     }
   }
@@ -49,7 +55,42 @@ pub fn solve(input: String) -> u32 {
     }
   }
 
-  p1
+  let mut p2 = 0;
+  for gp in gear_pos {
+    let mut num_marked = HashSet::<Pos>::new();
+    let mut gears = Vec::<u32>::new();
+    for p in surround(gp) {
+      if num_marked.contains(&p) {
+        continue;
+      }
+      num_marked.insert(p);
+
+      if let Some(n) = map.get(&p) {
+        let mut num_str = String::from(*n);
+        let mut p_front = (p.0, p.1 - 1);
+        while let Some(n) = map.get(&p_front) {
+          num_marked.insert(p_front);
+          num_str.insert(0, *n);
+          p_front.1 -= 1;
+        }
+
+        let mut p_end = (p.0, p.1 + 1);
+        while let Some(n) = map.get(&p_end) {
+          num_marked.insert(p_end);
+          num_str.push(*n);
+          p_end.1 += 1;
+        }
+
+        gears.push(num_str.parse::<u32>().unwrap());
+      }
+    }
+
+    if gears.len() == 2 {
+      p2 += gears[0] * gears[1];
+    }
+  }
+
+  (p1, p2)
 }
 
 fn surround(p: Pos) -> Vec<Pos> {
@@ -84,6 +125,6 @@ mod tests {
     .664.598..
     "#;
 
-    assert_eq!(4361, solve(input.to_string()));
+    assert_eq!((4361, 467835), solve(input.to_string()));
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod day03;
 
 fn main() {
   let content = std::fs::read_to_string("inputs/day03.txt").expect("read file");
-  let p1 = day03::solve(content);
+  let (p1, p2) = day03::solve(content);
 
   println!("part1: {}", p1);
+  println!("part2: {}", p2);
 }
