e33ea472f85768dfd7b014b565a6f59ed7387422
LOU Xun <aquarhead@gmail.com>
2023-12-09 10:02:29 +0000

    2023 day 9

---
+++ src/day09.rs
@@ -0,0 +1,58 @@
+use rayon::prelude::*;
+
+pub fn solve(input: &str) -> (i64, i64) {
+  let hists: Vec<_> = input
+    .trim()
+    .lines()
+    .map(|line| {
+      line
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<_>>()
+    })
+    .collect();
+
+  let p1 = hists.clone().into_par_iter().map(|h| p1(h)).sum();
+  let p2 = hists.into_par_iter().map(|h| p2(h)).sum();
+
+  (p1, p2)
+}
+
+fn p1(mut diffs: Vec<i64>) -> i64 {
+  let mut res = 0;
+  while diffs.iter().any(|x| *x != 0) {
+    for i in 0..(diffs.len() - 1) {
+      diffs[i] = diffs[i + 1] - diffs[i];
+    }
+    res += diffs.pop().unwrap();
+  }
+
+  res
+}
+
+fn p2(mut diffs: Vec<i64>) -> i64 {
+  let mut heads = Vec::new();
+  while diffs.iter().any(|x| *x != 0) {
+    heads.push(diffs[0]);
+    for i in 0..(diffs.len() - 1) {
+      diffs[i] = diffs[i + 1] - diffs[i];
+    }
+    let _ = diffs.pop();
+  }
+
+  heads.into_iter().rfold(0, |acc, x| x - acc)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_example() {
+    assert_eq!(18, p1(vec![0, 3, 6, 9, 12, 15]));
+    assert_eq!(28, p1(vec![1, 3, 6, 10, 15, 21]));
+    assert_eq!(68, p1(vec![10, 13, 16, 21, 30, 45]));
+
+    assert_eq!(5, p2(vec![10, 13, 16, 21, 30, 45]));
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 
 fn main() {
-  let content = std::fs::read_to_string("inputs/day08.txt").expect("read file");
-  let (p1, p2) = day08::solve(&content);
+  let content = std::fs::read_to_string("inputs/day09.txt").expect("read file");
+  let (p1, p2) = day09::solve(&content);
 
   println!("part1: {}", p1);
   println!("part2: {}", p2);
