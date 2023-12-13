19810df54ab17c8fa62f1872f5fb4e734bab3405
LOU Xun <aquarhead@gmail.com>
2023-12-13 21:31:19 +0000

    2023 day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -1,31 +1,36 @@
+use rayon::prelude::*;
+
 pub fn solve(input: &str) -> (u64, u64) {
-  let p1 = input
+  let ps = input
     .trim()
     .split("\n\n")
-    .map(|pattern| {
-      //
-      p1(pattern)
-    })
-    .sum();
+    .map(|x| x.to_string())
+    .collect::<Vec<_>>();
 
-  (p1, 0)
+  ps.into_par_iter()
+    .map(|p| {
+      let mut rows = Vec::new();
+      let mut cols = Vec::new();
+      for line in p.trim().lines() {
+        // first line
+        if cols.len() == 0 {
+          cols = vec![0; line.len()];
+        }
+        rows.push(line.chars().map(digit).fold(0, |acc, x| (acc << 1) + x));
+        for (i, x) in line.char_indices() {
+          cols[i] <<= 1;
+          cols[i] += digit(x);
+        }
+      }
+
+      let p1 = p1(&rows, &cols);
+
+      (p1, p2(rows, cols, p1))
+    })
+    .reduce(|| (0, 0), |acc, res| (acc.0 + res.0, acc.1 + res.1))
 }
 
-fn p1(pattern: &str) -> u64 {
-  let mut rows = Vec::new();
-  let mut cols = Vec::new();
-  for line in pattern.trim().lines() {
-    // first line
-    if cols.len() == 0 {
-      cols = vec![0; line.len()];
-    }
-    rows.push(line.chars().map(digit).fold(0, |acc, x| (acc << 1) + x));
-    for (i, x) in line.char_indices() {
-      cols[i] <<= 1;
-      cols[i] += digit(x);
-    }
-  }
-
+fn p1(rows: &[u64], cols: &[u64]) -> u64 {
   for s in 1..cols.len() {
     let (a, b) = cols.split_at(s);
     if a.into_iter().rev().zip(b).all(|(x, y)| x == y) {
@@ -37,6 +42,42 @@ fn p1(pattern: &str) -> u64 {
     let (a, b) = rows.split_at(s);
     if a.into_iter().rev().zip(b).all(|(x, y)| x == y) {
       return s as u64 * 100;
+    }
+  }
+
+  panic!("impossible")
+}
+
+fn p2(mut rows: Vec<u64>, mut cols: Vec<u64>, bad: u64) -> u64 {
+  for i in 0..rows.len() {
+    for j in 0..cols.len() {
+      let j = cols.len() - 1 - j;
+      rows[i] ^= 1 << j;
+      cols[j] ^= 1 << (rows.len() - 1 - i);
+
+      for s in 1..cols.len() {
+        let (a, b) = cols.split_at(s);
+        if a.into_iter().rev().zip(b).all(|(x, y)| x == y) {
+          let ret = s as u64;
+          if ret != bad {
+            return ret;
+          }
+        }
+      }
+
+      for s in 1..rows.len() {
+        let (a, b) = rows.split_at(s);
+        if a.into_iter().rev().zip(b).all(|(x, y)| x == y) {
+          let ret = s as u64 * 100;
+          if ret != bad {
+            return ret;
+          }
+        }
+      }
+
+      // revert
+      rows[i] ^= 1 << j;
+      cols[j] ^= 1 << (rows.len() - 1 - i);
     }
   }
 
@@ -74,6 +115,6 @@ mod tests {
 ..##..###
 #....#..#
 ";
-    assert_eq!((405, 0), solve(input));
+    assert_eq!((405, 400), solve(input));
   }
 }
